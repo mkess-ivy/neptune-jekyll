7df2e7c74b8f4d456d926d16f4ca259b184d73d1
Montier Kess <montier.kess@gmail.com>
2017-01-15 17:34:17 -0500

    Added back to top functionality

--- neptune-site/js/main.js
+++ neptune-site/js/main.js
@@ -5,6 +5,20 @@ jQuery(document).ready(function($) {
 		$('#preloader').fadeOut('slow',function(){$(this).remove();});
 	});
 
+	// Smooth Scroll
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
+
+		var target = this.hash;
+		var $target = $(target);
+
+		$('html, body').stop().animate({
+		  'scrollTop': $target.offset().top
+		}, 900, 'swing', function () {
+		  window.location.hash = target;
+		});
+  });
+
 });
 
 
